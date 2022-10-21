0dbaac1791d83f1124b62be6ae25e6e42edd9603
Moriatsushi <k021g1463@g.neec.ac.jp>
2022-10-21 15:29:46 +0900

    テクスチャと法線

--- Object3d.cpp
+++ Object3d.cpp
@@ -404,7 +404,7 @@ void Object3d::CreateModel()
 	//ファイルストリーム
 	std::ifstream file;
 	//.objファイルを開く
-	file.open("Resources/triangle/triangle.obj");
+	file.open("Resources/triangle/triangle_tex.obj");
 	//ファイルオープン失敗をチェック
 	assert(!file.fail());
 
@@ -431,9 +431,30 @@ void Object3d::CreateModel()
 			//座標データに追加
 			positions.emplace_back(position);
 			//頂点データに追加
-			VertexPosNormalUv vertex{};
+			/*VertexPosNormalUv vertex{};
 			vertex.pos = position;
-			vertices.emplace_back(vertex);
+			vertices.emplace_back(vertex);*/
+		}
+		//先頭頭文字列がVtなら法線ベクトル
+		if (key == "vt") {
+			//U,V成分読み込み
+			XMFLOAT2 texcoord{};
+			line_stream >> texcoord.x;
+			line_stream >> texcoord.y;
+			//V方向反転
+			texcoord.y = 1.0f - texcoord.y;
+			//テクスチャ座標データに追加
+			texcoords.emplace_back(texcoord);
+		}
+		//先頭頭文字列がVnなら法線ベクトル
+		if (key == "vn") {
+			//X,Y,Z座標読み込み
+			XMFLOAT3 normal{};
+			line_stream >> normal.x;
+			line_stream >> normal.y;
+			line_stream >> normal.z;
+			//法線ベクトルデータに追加
+			normals.emplace_back(normal);
 		}
 		//先頭頭文字列がfならポリゴン（三角形）
 		if (key == "f") {
@@ -442,10 +463,20 @@ void Object3d::CreateModel()
 			while (getline(line_stream, index_string, ' ')) {
 				//頂点インデックス１個分の文字列をストリームに変換して解析すしやすくなる
 				std::istringstream index_stream(index_string);
-				unsigned short indexPosition;
+				unsigned short indexPosition, indexNormal, indexTexcoord;
 				index_stream >> indexPosition;
-				//頂点インデックスに追加
-				indices.emplace_back(indexPosition - 1);
+				index_stream.seekg(1, ios_base::cur);//スラッシュを飛ばす
+				index_stream >> indexTexcoord;
+				index_stream.seekg(1, ios_base::cur);//スラッシュを飛ばす
+				index_stream >> indexNormal;
+				//頂点データの追加
+				VertexPosNormalUv vertex{};
+				vertex.pos = positions[indexPosition - 1];
+				vertex.normal = normals[indexNormal - 1];
+				vertex.uv = texcoords[indexTexcoord - 1];
+				vertices.emplace_back(vertex);
+				//インデックスデータの追加
+				indices.emplace_back((unsigned short)indices.size());
 			}
 		}
 	}
